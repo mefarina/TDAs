60edbfd40aa484eb4e241b829a79743750649978
mefarina <melissa.a.farina@gmail.com>
2022-04-01 01:33:51 -0300

    Primitivas de la lista

---
+++ lista_enlazada/lista.c
@@ -0,0 +1,96 @@
+#include "lista.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct nodo nodo_t; 
+
+struct nodo {
+    void* dato;
+    nodo_t* prox;
+};
+
+struct lista {
+    nodo_t* primero;
+    nodo_t* ultimo;
+    size_t largo;
+};
+
+lista_t *lista_crear(void) {
+    lista_t* lista = malloc(sizeof(lista_t));
+
+    if (lista == NULL) {
+        return NULL;
+    }
+
+    lista->primero = NULL;
+    lista->ultimo = NULL;
+    lista->largo = 0;
+
+    return lista;
+}
+
+bool lista_esta_vacia(const lista_t *lista) {
+    return lista->largo == 0;
+}
+
+
+bool lista_insertar_primero(lista_t *lista, void *valor) {
+    nodo_t* nodo = malloc(sizeof(nodo_t));
+
+    if (nodo == NULL) {
+        return false;
+    }
+
+	nodo->dato = valor;
+	nodo->prox = lista->primero;
+
+	if (lista_esta_vacia(lista) == true) {
+		lista->ultimo = nodo;
+	}
+	lista->primero = nodo;
+	lista->largo++;
+
+    return true;
+}
+
+
+void *lista_ver_primero(const lista_t *lista){
+	return lista->primero->dato;
+}
+
+
+void *lista_borrar_primero(lista_t *lista) {
+	if (lista_esta_vacia(lista)) {
+		return NULL;
+	}
+
+	nodo_t* nodo_actual = lista->primero;
+	void* dato_actual = lista->primero->dato;
+	lista->primero = lista->primero->prox;
+	
+	free(nodo_actual);
+	return dato_actual;
+}
+
+bool lista_insertar_ultimo(lista_t *lista, void *valor){
+	nodo_t* nodo = malloc(sizeof(nodo_t));
+
+	if (nodo == NULL) {
+		return false;
+	}
+	nodo->dato = valor;
+	lista->ultimo->prox = nodo;
+	lista->ultimo = nodo;
+
+	lista->largo++;
+	return true;
+}
+
+
+void *lista_ver_ultimo(const lista_t *lista) {
+	return lista->ultimo->dato;
+}
+
+size_t lista_largo(const lista_t *lista) {
+	return lista->largo;
+}
---
+++ lista_enlazada/lista.h
@@ -0,0 +1,83 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+#include <stdbool.h>
+
+
+/* ******************************************************************
+ *                DEFINICION DE LOS TIPOS DE DATOS
+ * *****************************************************************/
+
+/* La lista está planteada como una lista de punteros genéricos. */
+
+struct lista;
+typedef struct lista lista_t;
+
+
+/* ******************************************************************
+ *                    PRIMITIVAS DE LA LISTA
+ * *****************************************************************/
+
+// Crea una lista.
+// Post: devuelve una nueva lista vacía.
+lista_t *lista_crear(void);
+
+// Destruye la lista. Si se recibe la función destruir_dato por parámetro,
+// para cada uno de los elementos de la lista llama a destruir_dato.
+// Pre: la lista fue creada. destruir_dato es una función capaz de destruir
+// los datos de la lista, o NULL en caso de que no se la utilice.
+// Post: se eliminaron todos los elementos de la lista.
+void lista_destruir(lista_t *lista, void (*destruir_dato)(void *));
+
+// Devuelve verdadero si la lista no tiene elementos enlistados, false en caso contrario.
+// Pre: la lista fue creada.
+bool lista_esta_vacia(const lista_t *lista);
+
+// Agrega un nuevo elemento al principio la lista. Devuelve falso en caso de error.
+// Pre: la lista fue creada.
+// Post: se agregó un nuevo elemento a la lista, valor se encuentra al final
+// de la lista.
+bool lista_insertar_primero(lista_t *lista, void *valor);
+
+// Agrega un nuevo elemento al final de la lista. Devuelve falso en caso de error.
+// Pre: la lista fue creada.
+// Post: se agregó un nuevo elemento a la lista, valor se encuentra al final
+// de la lista.
+bool lista_insertar_ultimo(lista_t *lista, void *valor);
+
+// Obtiene el valor del primer elemento de la lista. Si la lista tiene
+// elementos, se devuelve el valor del primero, si está vacía devuelve NULL.
+// Pre: la lista fue creada.
+// Post: se devolvió el primer elemento de la lista, cuando no está vacía.
+void *lista_ver_primero(const lista_t *lista);
+
+
+// Obtiene el valor del ultimo elemento de la lista. Si la lista tiene
+// elementos, se devuelve el valor del primero, si está vacía devuelve NULL.
+// Pre: la lista fue creada.
+// Post: se devolvió el primer elemento de la lista, cuando no está vacía.
+void *lista_ver_ultimo(const lista_t *lista);
+
+// Saca el primer elemento de la lista. Si la lista tiene elementos, se quita el
+// primero de la lista, y se devuelve su valor, si está vacía, devuelve NULL.
+// Pre: la lista fue creada.
+// Post: se devolvió el valor del primer elemento anterior, la lista
+// contiene un elemento menos, si la lista no estaba vacía.
+void *lista_borrar_primero(lista_t *lista);
+
+
+/* *****************************************************************
+ *                      PRUEBAS UNITARIAS
+ * *****************************************************************/
+
+// Realiza pruebas sobre la implementación realizada.
+//
+// Las pruebas deben emplazarse en el archivo ‘pruebas_lista.c’, y
+// solamente pueden emplear la interfaz pública tal y como aparece en lista.h
+// (esto es, las pruebas no pueden acceder a los miembros del struct lista).
+//
+// Para la implementación de las pruebas se debe emplear la función
+// print_test(), como se ha visto en TPs anteriores.
+void pruebas_lista_estudiante(void);
+
+#endif  // LISTA_H
